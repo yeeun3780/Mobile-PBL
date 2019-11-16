32d357d0c77be569330940d361a5437e55c23712
ChoiYuJin <cyj2857@naver.com>
2019-11-16 22:26:26 +0900

    TermProject

---
+++ TeemProject_Tutong/Tuting_Login/app/src/main/java/com/hausung/hstuting/MainActivity.java
@@ -0,0 +1,40 @@
+package com.hausung.hstuting;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button registerBtn, loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeViews();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initializeViews() {
+        registerBtn = findViewById(R.id.register);
+        loginBtn = findViewById(R.id.login);
+    }
+}
